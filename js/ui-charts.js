53ceb52fe6824e3ead433c77e617e778e966a061
Ioan <ioan.orzetic@gmail.com>
2015-07-02 16:05:52 +0300

    changes for the WN chart: labels, marker, sizes, colors etc.

--- js/ui-charts.js
+++ js/ui-charts.js
@@ -1,14 +1,12 @@
 //Holds a 365 length array of a weather measurement
-function ChartSeries(startDate)
-{
+function ChartSeries(startDate) {
 	this.startDate = startDate;
 	this.data = new Array(365);
 
 	for (var i = 0; i < this.data.length; this.data[i] = null, i++);
 }
 
-ChartSeries.prototype.insertAtDate = function(dateStr, value)
-{
+ChartSeries.prototype.insertAtDate = function(dateStr, value) {
 	var index = Util.getDateIndex(dateStr, this.startDate);
 
 	if (index < 0 || index >= 365)
@@ -21,18 +19,17 @@ ChartSeries.prototype.insertAtDate = function(dateStr, value)
 	return true;
 }
 
-function ChartData()
-{
-    this.days = [];
+function ChartData() {
+	this.days = [];
 
-    var end = new Date();
-    end.setDate(end.getDate() + 7); //Forecast for 7 days in the future
+	var end = new Date();
+	end.setDate(end.getDate() + 7); //Forecast for 7 days in the future
 
 	this.startDate = new Date(end);
 	this.startDate.setFullYear(end.getFullYear() - 1);
 
-	//Fill a 356 array with dates
-    var _start = new Date(this.startDate);
+//Fill a 356 array with dates
+	var _start = new Date(this.startDate);
 	while (_start < end)
 	{
 		var isoDate = _start.toISOString().split("T")[0];
@@ -41,79 +38,82 @@ function ChartData()
 	}
 
 	this.qpf = new ChartSeries(this.startDate);
-    this.maxt= new ChartSeries(this.startDate);
-    this.mint= new ChartSeries(this.startDate);
-    this.waterNeed = new ChartSeries(this.startDate);
+	this.maxt = new ChartSeries(this.startDate);
+	this.mint = new ChartSeries(this.startDate);
+	this.waterNeed = new ChartSeries(this.startDate);
 	this.condition = new ChartSeries(this.startDate);
-	this.conditionMap = {};
+
+	this.chartDataMap = {};
+
 	this.programs = [];
 
 	console.log("Initialised ChartData from %s to %s",this.startDate.toDateString(), end.toDateString());
 }
 
-function normalizeWaterNeed(user, scheduled)
-{
+function normalizeWaterNeed(user, scheduled) {
 	var wn = 0;
-	if (scheduled <= 0 && user > 0)
+	if (scheduled <= 0 && user > 0) {
 		wn = 100;
-	else if (scheduled == 0 && user == 0)
+	} else if (scheduled == 0 && user == 0) {
 		wn = 0;
-	else
+	} else {
 		wn = Math.round((user / scheduled) * 100);
+	}
 
 	return wn;
 }
 
 // return the character from the TTF font containing weather icons
-function conditionToGlyph(condition)
-{
-    return;
+function conditionToGlyph(condition) {
+	return;
 }
 
 var chartData = new ChartData();
 
-function generateCharts()
-{
+function generateCharts() {
 	Data.mixerData = API.getMixer();
 	Data.dailyDetails = API.getDailyStats(null, true);
 
 	//Get all available days in mixer TODO: Can be quite long (365 days)
-	for (var i = 0; i < Data.mixerData.mixerData.length; i++)
-	{
+	for (var i = 0; i < Data.mixerData.mixerData.length; i++) {
 		var recent = Data.mixerData.mixerData[i].dailyValues;
-		for (var j = 0; j < recent.length; j++)
-    	{
-    		var day =  recent[j].day.split(' ')[0];
-    		chartData.qpf.insertAtDate(day, recent[j].qpf);
-            chartData.maxt.insertAtDate(day, recent[j].maxTemp);
-            chartData.mint.insertAtDate(day, recent[j].minTemp);
-            chartData.condition.insertAtDate(day, recent[j].condition);
-            chartData.conditionMap[day] = recent[j].condition;
-    	}
+
+		for (var j = 0; j < recent.length; j++) {
+			var day =  recent[j].day.split(' ')[0];
+			chartData.qpf.insertAtDate(day, recent[j].qpf);
+			chartData.maxt.insertAtDate(day, recent[j].maxTemp);
+			chartData.mint.insertAtDate(day, recent[j].minTemp);
+			chartData.condition.insertAtDate(day, recent[j].condition);
+
+			chartData.chartDataMap[day] = {
+				qpf: recent[j].condition,
+				maxt: recent[j].maxTemp,
+				mint: recent[j].minTemp,
+				condition: recent[j].condition
+			};
+		}
 	}
 
 	//Total Water Need
 	var maxWN = 100;
 	var daily = Data.dailyDetails.DailyStatsDetails;
 
-	for (var i = 0; i < daily.length; i++)
-	{
+	for (var i = 0; i < daily.length; i++) {
 		var totalDayUserWater = 0;
 		var totalDayScheduledWater = 0;
 		//programs for the day
-		for (var p = 0; p < daily[i].programs.length; p++)
-		{
+		for (var p = 0; p < daily[i].programs.length; p++) {
 			var cp = daily[i].programs[p];
 
 			// Program index not in our struct ?
-            if (p > chartData.programs.length - 1)
-            	pIndex = chartData.programs.push(new ChartSeries(chartData.startDate)) - 1;
-            else
-            	pIndex = p;
+			if (p > chartData.programs.length - 1) {
+				pIndex = chartData.programs.push(new ChartSeries(chartData.startDate)) - 1;
+			} else {
+				pIndex = p;
+			}
 
 			//zones for the programs
-			for (var z = 0; z < cp.zones.length; z++)
-			{
+			for (var z = 0; z < cp.zones.length; z++) {
 				totalDayUserWater += cp.zones[z].computedWateringTime;
 				totalDayScheduledWater += cp.zones[z].scheduledWateringTime;
 				//console.log("User: %d, Scheduled: %d", totalDayUserWater, totalDayScheduledWater);
@@ -124,87 +124,121 @@ function generateCharts()
 		}
 
 		var dailyWN = normalizeWaterNeed(totalDayUserWater, totalDayScheduledWater);
-		if (dailyWN > maxWN)
+		if (dailyWN > maxWN) {
 			maxWN = dailyWN;
+		}
 
-		chartData.waterNeed.insertAtDate(daily[i].day, dailyWN);
+		// if the day is the current date than mark the point with a different color
+		if (daily[i].day === Highcharts.dateFormat('%Y-%m-%d', new Date())) {
+			chartData.waterNeed.insertAtDate(daily[i].day, {
+				color: 'red',
+				y: dailyWN
+			});
+		} else {
+			chartData.waterNeed.insertAtDate(daily[i].day, dailyWN);
+		}
 	}
 
 	var daysSlice = -14; //2 weeks
 
 	makeVisibleBlock($("#dashboard"));
 
+	var waterNeedChartPrimaryAxisCategories = chartData.days.slice(daysSlice);
+
 	var waterNeedChart = new Highcharts.Chart({
 		chart: {
 			renderTo: 'chartWaterNeed',
-			margin: [90, 70, 90, 70]
-		},
-		title: {
-			text: '',
-			x: -20 //center
-		},
-		xAxis: [{
-			offset: -310,
-			tickWidth: 0,
-			lineWidth: 0,
-			categories: chartData.days.slice(daysSlice),
-			labels: {
-				x: -10,
-				useHTML: true,
-				style: {"font-family": "RainMachine", "font-size": "42px"},
-				formatter: function () {
-					//Our condition mapping in TTF front
-					var condition = chartData.conditionMap[this.value];
-					if (condition === undefined)
-						return String.fromCharCode(122);
-
-					return String.fromCharCode(97 + condition);
-				}
-			},
-		}, {
-			linkedTo: 0,
-			categories: chartData.days.slice(daysSlice),
-		}],
-
-		yAxis: {
-			title: {
-				text: 'Water Need (%)'
-			},
-			stackLabels: {
-				style: {
-                	"color": "black",
-                	"font-size": "14px",
-                },
-				formatter: function () {
-					return this.total + " %";
-				},
-                enabled: false
-            },
-			min: 0,
-			max: maxWN,
-			plotLines: [{
-				value: 0,
-				width: 1,
-				color: '#808080'
-			}]
+			spacingTop: -40
 		},
 		plotOptions: {
 			column: {
-				stacking: 'normal',
 				dataLabels: {
+					enabled: true,
 					formatter: function () {
-						return this.total + " %";
-					},
-					enabled: true
-				}
+						return this.total + "%";
+					}
+				},
+				stacking: 'normal'
 			}
 		},
-
 		series: [{
-			type: 'column',
+			data: chartData.waterNeed.data.slice(daysSlice),
+			dataLabels: {
+				enable: true
+			},
+			events: {
+				legendItemClick: function() {
+					return false;
+				}
+			},
 			name: 'Water Need',
-			data: chartData.waterNeed.data.slice(daysSlice)
-		}]
+			tooltip: {
+				headerFormat: '',
+				pointFormatter: function () {
+					return '<span style="font-size: 12px;">' + Highcharts.dateFormat('%b %e', new Date(this.category)) + '</span>: <span style="font-size: 14px;">' + this.y + '%</span>';
+				}
+			},
+			type: 'column'
+		}],
+		title: false,
+		xAxis: [{
+			categories: waterNeedChartPrimaryAxisCategories,
+			labels: {
+				formatter: function () {
+					return Highcharts.dateFormat('%b %e', new Date(this.value));
+				},
+				style: {
+					"color": "#808080",
+					"font-size": "10px",
+				}
+			}
+		}, {
+			categories: waterNeedChartPrimaryAxisCategories,
+			labels: {
+				formatter: function () {
+					//Our condition mapping in TTF front
+					var condition = chartData.chartDataMap[this.value].condition,
+						temperatureValue = Math.round(chartData.chartDataMap[this.value].maxt),
+						conditionValue;
+
+					if (condition === undefined) {
+						conditionValue = String.fromCharCode(122);
+					} else {
+						conditionValue = String.fromCharCode(97 + condition);
+					}
+
+					return '<span style="font-family: RainMachine; font-size: 42px;">' + conditionValue + '</span>' +
+						'<br />' +
+						'<span style="font-size: 16px; line-height: 24px;">' + temperatureValue + '</span>';
+				},
+				style: {
+					"color": "#808080",
+					"text-align": "center"
+				},
+				useHTML: true,
+				x: -10
+			},
+			lineWidth: 0,
+			linkedTo: 0,
+			offset: 50,
+			opposite: true,
+			tickWidth: 0
+		}],
+		yAxis: {
+			max: maxWN,
+			min: 0,
+			labels: {
+				enabled: true,
+				formatter: function () {
+					return this.value + "%";
+				},
+				style: {
+					"color": "#808080",
+					"font-size": "10px",
+				}
+			},
+			title: false
+		}
 	});
 
 	var qpfChart = new Highcharts.Chart({
@@ -280,38 +314,37 @@ function generateCharts()
 		div.id = "programChart-" + c;
 		div.className = "charts";
 
-		var tmpChart = new Highcharts.Chart(
-			{
-				chart: {
-					renderTo: div.id,
-					marginRight: 0
-				},
-				title: {
-					text: 'Program ' + c + " Water Need",
-					x: -20 //center
-				},
-				xAxis: [{
-					categories: chartData.days.slice(daysSlice),
-				}],
+		var tmpChart = new Highcharts.Chart({
+			chart: {
+				renderTo: div.id,
+				marginRight: 0
+			},
+			title: {
+				text: 'Program ' + c + " Water Need",
+				x: -20 //center
+			},
+			xAxis: [{
+				categories: chartData.days.slice(daysSlice),
+			}],
 
-				yAxis: {
-					title: {
-						text: 'Water Need (%)'
-					},
-					min: 0,
-					max: maxWN,
-					plotLines: [{
-						value: 0,
-						width: 1,
-						color: '#808080'
-					}]
+			yAxis: {
+				title: {
+					text: 'Water Need (%)'
 				},
-				series: [{
-					type: 'column',
-					name: 'Program ' + c,
-					data: chartData.programs[c].data.slice(daysSlice)
+				min: 0,
+				max: maxWN,
+				plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
 				}]
-			});
+			},
+			series: [{
+				type: 'column',
+				name: 'Program ' + c,
+				data: chartData.programs[c].data.slice(daysSlice)
+			}]
+		});
 
 		programsCharts.push(tmpChart);
 	}
