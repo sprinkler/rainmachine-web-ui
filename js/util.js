ebf9c47687fb8c09b0feb0cf1ca32e0c098e892a
Nicu Pavel <npavel@mini-box.com>
2016-08-16 14:02:34 +0300

    Parsers/Util: Parse of JSON as string received from parameters

--- js/util.js
+++ js/util.js
@@ -264,56 +264,71 @@ Util.isFloat = function(value) {
  * @param label - label for the data
  * @returns the newly created div
  */
+
 Util.generateTagFromDataType = function(parent, data, label) {
-	var div = addTag(parent, 'div');
-	div.className = "generatedTag";
+	var divElem = addTag(parent, 'div');
+	var labelElem = addTag(divElem, 'label');
+	var inputElem;
 
+	var id = "generated-" + label;
 	var isReadOnly = label.startsWith("_");
+	var dataFromJSON = null;
 
-	if (! isReadOnly) {
-		div.textContent = label;
-		var input = addTag(div, 'input');
-		input.id = "generated-" + label;
-		input.type = "text"; //default type for null, object, number or string types
+	divElem.className = "generatedTag";
 
+	//Check if we got a string with json data
+	try {
+		dataFromJSON = JSON.parse(data);
+	} catch(e) {}
 
-		if (typeof data == "boolean") {
-			input.type = "checkbox";
-			if (data) {
-				input.checked = true;
+	if (!isReadOnly) {
+		labelElem.htmlFor = id;
+		labelElem.textContent = label;
+
+		if (data === null || (typeof data == "string" && dataFromJSON === null) || typeof data == "number" || typeof data == "boolean") {
+			inputElem = addTag(divElem, 'input');
+			inputElem.id = id;
+			inputElem.type = "text"; //default type for null, object, number or string types
+			if (typeof data == "boolean") {
+				inputElem.type = "checkbox";
+				if (data) {
+					inputElem.checked = true;
+				}
+			} else {
+				inputElem.value = data;
+				inputElem.className = "typeText";
 			}
-		} else if (data !== null && typeof data == "object") {
-			input = addTag(div, 'textarea');
-			input.value = JSON.stringify(data);
-			input.className = "typeText";
-		} else	{
-			input.value = data;
-			input.className = "typeText";
+		} else {
+			inputElem = addTag(divElem, 'textarea');
+			if (typeof data == "object") {
+				inputElem.value = JSON.stringify(data, null, 4);
+				console.log("%s: JS Object detected", label);
+			} else if (dataFromJSON !== null) { // Check if we received JSON data as string
+				inputElem.value = JSON.stringify(dataFromJSON, null, 4);
+				console.log("%s: JSON data as string detected", label);
+			}
 		}
 	} else {
-		div.textContent = label.substr(1);
-		var input = addTag(div, 'div');
+		divElem.textContent = label.substr(1);
+		var inputElem = addTag(divElem, 'div');
 
 		if (data instanceof Array) {
 			for (var d in data) {
-				input.innerHTML += data[d] + "<br>";
+				inputElem.innerHTML += data[d] + "<br>";
 			}
 		} else if (data !== null && typeof data == "object") {
-			input.textContent = JSON.stringify(data);
+			inputElem.textContent = JSON.stringify(data, null, 4);
 		} else {
-			input.textContent = data;
+			inputElem.textContent = data;
 		}
 	}
 
-    return div;
-}
+    return divElem;
+};
 
 /**
- * Generates a div with a label and an input tag depending on data type
- * @param parent - parent element for the newly created div
- * @param data - data
- * @param label - label for the data
- * @returns {array} - in form [label, value]
+ * Reads the value of a generated tag with Util.generateTagFromDataType
+ * @param label - the label string of the tag which is used to construct the tag id
  */
 
 Util.readGeneratedTagValue = function(label) {
