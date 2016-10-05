3781f6e7107c48869b1293b814169a528b78ce4a
Nicu Pavel <npavel@mini-box.com>
2016-10-05 12:58:37 +0300

    Help: Add more help for zones properties

--- js/help.js
+++ js/help.js
@@ -16,9 +16,21 @@ var Help = (function(Help) {
 		"programPastValues": "The differences of EvapoTranspiration and Precipitation between the day best forecast " +
 							"(seen on the left) and known forecast at the time of program run. " +
 							"Big differences (more than 2mm) can be solved by enabling Correction for the Past in Settings.",
+		"zoneAllowedDepletion": "The maximum percentage of the soil moisture/water that is allowed to be consume before " +
+		"					being replenished by irrigation.",
+		"zoneRootDepth": "The root depth of the plant from which it extracts the most of the needed water.",
+		"zoneWiltingPoint": "The minimum percentage of soil moisure/water before the plants will wilt and fail to recover.",
+		"zoneTallPlant": "Taller plants usually lose more water with transpiration versus shorter plants",
+		"zoneCropCoef": "For Multiple monthly values, is a list of evapotranspiration percentage from the total evaporation " +
+						"plant potential depending on growing patterns. For Single yearly value, it's an average of yearly values.",
+		"zoneIntakeRate": "The rate at which soil is able to absorb rainfall or irrigation, if this value is below the " +
+							"sprinkler head precipitation rate, RainMachine will automatically split the watering in cycles " +
+							"to prevent runoff.",
+		"zoneFieldCapacity": "The percentage of water remaining in the soil after the soil has been saturated (100%) and allowed to drain away",
 		"zonePrecipRate": "The quantitative precipitation that the sprinkler head can deposit per hour. This value can be " +
 							"calculated either by a catch cup test or by zone water meter flow and area",
 		"zoneAppEfficiency": "The ratio of the average water depth applied and the target water depth during an irrigation event.",
+		"zoneSurfaceAccumulation": "The maximum height of water that is allowed to accumulate over the soil surface before runoff occurs",
 		"waterSavedVolume": "For accurate results make sure you enter the correct precipitation rate, zone area or zone total flow in advanced zone settings."
 	};
 
