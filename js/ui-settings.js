9ada00ec2455bcae5f7545c4c6062764e1d7ca1c
Nicu Pavel <npavel@mini-box.com>
2018-11-08 03:40:50 -0800

    Renamed PWS Parser to WeatherLink

--- js/ui-settings.js
+++ js/ui-settings.js
@@ -30,7 +30,7 @@ window.ui = window.ui || {};
 	var developerParsers = {
 		"My Example Parser": 1,
 		"Fixed Parser": 1,
-		"PWS Parser": 1,
+		"WeatherDisplay Parser": 1,
 		"Simulator Parser": 1,
 		"Weather Rules Parser": 1,
 		"Local Weather Push Parser": 1
