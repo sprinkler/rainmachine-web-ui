dd75da7b33ea6d2d4b51c27f6cb67803a2310da4
Iulian Luca <lucaiuli@gmail.com>
2021-02-16 15:39:07 +0200

    Revert "Revert "Fixed weather parsers buttons and badges.""
    This reverts commit e3884a8be18b5f32d05f349c142612fb1a01e3ff.

--- js/dom.js
+++ js/dom.js
@@ -167,7 +167,7 @@ function makeVisible(tag)
 	if (typeof(tag) === 'string') { e = $(tag); }
     else { e = tag; }
 
-	e.style.display = "inline";
+	e.style.display = "block";
 	//correct display highchart graphs
 	e.style.width = "100%";
 	//e.focus();
